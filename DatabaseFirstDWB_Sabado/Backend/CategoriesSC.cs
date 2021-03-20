3d06c5b9384cd244ad3835391dbbfba5c8bd8328
DonPPYLozGloboz <66177724+DonPPYLozGloboz@users.noreply.github.com>
2021-03-20 16:38:29 -0600

    Add files via upload

---
+++ DatabaseFirstDWB_Sabado/Backend/CategoriesSC.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseFirstDWB_Sabado.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseFirstDWB_Sabado.Backend
+{
+    class CategoriesSC
+    {
+        NorthwindContext dataContext = new NorthwindContext();
+
+        public IQueryable<Categories> GetAllCategories()
+        {
+            return dataContext.Categories.Select(s => s);
+        }
+    }
+}
