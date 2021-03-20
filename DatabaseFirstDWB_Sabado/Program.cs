70e7544a70e6d1ce9aeaac2121c180e7f1fc0809
DonPPYLozGloboz <66177724+DonPPYLozGloboz@users.noreply.github.com>
2021-03-20 16:39:27 -0600

    Add files via upload

---
+++ DatabaseFirstDWB_Sabado/Program.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseFirstDWB_Sabado.Backend;
+using System;
+using System.Linq;
+
+namespace DatabaseFirstDWB_Sabado
+{
+    class Program
+    {
+        public static CategoriesSC categoriesService = new CategoriesSC();
+
+        public static void Tarea2()
+        {
+            var output = categoriesService.GetAllCategories().ToList();
+        }
+        static void Main(string[] args)
+        {
+            Tarea2();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
